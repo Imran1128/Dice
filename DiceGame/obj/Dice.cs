16a570f85cbabb3f4fd1bf3fff338b128bf23198
Imran1128 <138041710+Imran1128@users.noreply.github.com>
2024-11-17 16:06:26 +0600

    gh

---
+++ DiceGame/obj/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceGame
+{
+    public class Dice
+    {
+        public List<int> Sides { get; }
+        private static Random rand = new Random();
+
+        public Dice(List<int> sides)
+        {
+            Sides = sides;
+        }
+
+        public int Roll()
+        {
+            int index = rand.Next(Sides.Count);
+            return Sides[index];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", Sides);
+        }
+    }
+}
---
+++ DiceGame/obj/ProbabilityCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceGame
+{
+    public class ProbabilityCalculator
+    {
+        public static void DisplayWinningProbabilities(List<Dice> userDice, List<Dice> computerDice)
+        {
+            Console.WriteLine("Winning probabilities table:");
+            Console.WriteLine("User Dice | Computer Dice | Probability of Winning");
+
+            foreach (var userDie in userDice)
+            {
+                foreach (var computerDie in computerDice)
+                {
+                    double probability = CalculateProbability(userDie, computerDie);
+                    Console.WriteLine($"{string.Join(",", userDie.Sides)} | {string.Join(",", computerDie.Sides)} | {probability:F2}");
+                }
+            }
+        }
+
+        private static double CalculateProbability(Dice userDie, Dice computerDie)
+        {
+            int userWins = 0;
+            int totalRolls = 0;
+
+            foreach (var userSide in userDie.Sides)
+            {
+                foreach (var computerSide in computerDie.Sides)
+                {
+                    totalRolls++;
+                    if (userSide > computerSide)
+                        userWins++;
+                }
+            }
+
+            return (double)userWins / totalRolls;
+        }
+    }
+}
